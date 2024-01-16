9b1fa44076a1504d9d5def122215574fe4643774
Mikey Ferguson <mikeyferg49@gmail.com>
2024-01-16 14:32:10 -0600

    Implemented RandomizedCollection and RandomizedSet

---
+++ LeetCode/src/RandomizedCollection.java
@@ -0,0 +1,91 @@
+import java.util.HashMap;
+import java.util.HashSet;
+/**
+ * RandomizedCollection is a data structure that contains a collection of
+ * numbers, possibly duplicates (i.e., a multiset). It should support inserting
+ * and removing specific elements and also reporting a random element.
+ * 
+ * Implement the RandomizedCollection class:
+ * - RandomizedCollection() Initializes the empty RandomizedCollection object.
+ * - bool insert(int val) Inserts an item val into the multiset, even if the item
+ *  is already present. Returns true if the item is not present, false otherwise.
+ * - bool remove(int val) Removes an item val from the multiset if present.
+ *  Returns true if the item is present, false otherwise. Note that if val has
+ *  multiple occurrences in the multiset, we only remove one of them.
+ * - int getRandom() Returns a random element from the current multiset of
+ *  elements. The probability of each element being returned is linearly related
+ *  to the number of the same values the multiset contains.
+ * 
+ * You must implement the functions of the class such that each function works
+ * on average O(1) time complexity.
+ * 
+ * Note: The test cases are generated such that getRandom will only be called if
+ * there is at least one item in the RandomizedCollection.
+ * 
+ * Link:
+ * https://leetcode.com/problems/insert-delete-getrandom-o1-duplicates-allowed/description/
+ */
+public class RandomizedCollection {
+
+    int count;
+    private HashMap<Integer, HashSet<Integer>> valueToIndices;
+    private HashMap<Integer, Integer> indexToValue;
+    private HashMap<Integer, Integer> valueToCount;
+    
+    public RandomizedCollection() {
+        count = 0;
+        valueToIndices = new HashMap<>();
+        indexToValue = new HashMap<>();
+        valueToCount = new HashMap<>();
+    }
+
+    public boolean insert(int val) {
+        if (valueToIndices.keySet().contains(val)) {
+            valueToIndices.get(val).add(count);
+            indexToValue.put(count, val);
+            valueToCount.put(val, valueToCount.get(val)+1);
+            count++;
+            return false;
+        } else {
+            valueToIndices.put(val, new HashSet<>());
+            valueToIndices.get(val).add(count);
+            indexToValue.put(count, val);
+            valueToCount.put(val, 1);
+            count++;
+            return true;
+        }
+    }
+
+    public boolean remove(int val) {
+        if (!valueToCount.keySet().contains(val))
+            return false;
+
+        count--;
+        int index = valueToIndices.get(val).iterator().next();
+        valueToIndices.get(val).remove(index);
+        if (valueToIndices.get(val).isEmpty()) { // that was the last instance of this element
+            valueToIndices.remove(val);
+            valueToCount.remove(val);
+            indexToValue.remove(index);
+        } else {
+            valueToCount.put(val, valueToCount.get(val)-1);
+            valueToIndices.get(val).remove(index);
+            indexToValue.remove(index);
+        }
+        // now we need to do the shifting
+        if (index < count) { // then we need to shift the "last" element in terms of our indexing
+            int valueToMove = indexToValue.get(count);
+            indexToValue.remove(count);
+            indexToValue.put(index, valueToMove);
+            valueToIndices.get(valueToMove).remove(count);
+            valueToIndices.get(valueToMove).add(index);
+        } // otherwise we just removed the "last" element and do not need to make any other changes
+        return true;
+    }
+
+    public int getRandom() {
+        int idx = (int)(count * Math.random());
+        return indexToValue.get(idx);
+    }
+
+}
---
+++ LeetCode/src/RandomizedSet.java
@@ -0,0 +1,67 @@
+import java.util.HashMap;
+/**
+ * Implement the RandomizedSet class:
+ * 
+ * - RandomizedSet() Initializes the RandomizedSet object.
+ * - bool insert(int val) Inserts an item val into the set if not present.
+ * Returns
+ * true if the item was not present, false otherwise.
+ * - bool remove(int val) Removes an item val from the set if present. Returns
+ * true if the item was present, false otherwise.
+ * - int getRandom() Returns a random element from the current set of elements
+ * (it's guaranteed that at least one element exists when this method is
+ * called). Each element must have the same probability of being returned.
+ * 
+ * You must implement the functions of the class such that each function works
+ * in average O(1) time complexity.
+ * 
+ * Link:
+ * https://leetcode.com/problems/insert-delete-getrandom-o1/description/?envType=daily-question&envId=2024-01-16
+ */
+public class RandomizedSet {
+
+    int count;
+    private HashMap<Integer, Integer> indexToValues;
+    private HashMap<Integer, Integer> valuesToIndex;
+    
+    public RandomizedSet() {
+        count = 0;
+        indexToValues = new HashMap<>();
+        valuesToIndex = new HashMap<>();
+    }
+
+    public boolean insert(int val) {
+        if (valuesToIndex.keySet().contains(val))
+            return false;
+
+        int index = count;
+        indexToValues.put(index, val);
+        valuesToIndex.put(val, index);
+        count++;
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if (!valuesToIndex.keySet().contains(val))
+            return false;
+
+        int index = valuesToIndex.get(val);
+        valuesToIndex.remove(val);
+        indexToValues.remove(index);
+        count--;
+        if (index < count) {
+            int valueToMove = indexToValues.get(count);
+            valuesToIndex.remove(valueToMove);
+            indexToValues.remove(count);
+            indexToValues.put(index, valueToMove);
+            valuesToIndex.put(valueToMove, index);
+        } // otherwise, we just removed the "last" value and we're fine as far as shifting the underlying map goes
+        return true;
+    }
+
+    public int getRandom() {
+        int idx = (int)(Math.random() * count);
+        return indexToValues.get(idx);
+    }
+
+}
