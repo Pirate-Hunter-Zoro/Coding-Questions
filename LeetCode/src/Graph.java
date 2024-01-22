c398be71e07709e2169a1419d8ff567c78b2a1b2
Mikey Ferguson <mikeyferg49@gmail.com>
2024-01-22 17:36:26 -0600

    Implemented Djikstra's shortest path in the Graph class

--- LeetCode/src/Graph.java
+++ LeetCode/src/Graph.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -31,24 +32,13 @@ import java.util.TreeSet;
 public class Graph {
 
     public static void main(String[] args) {
-        Graph graph = new Graph(13,
-                new int[][] { { 7, 2, 131570 }, { 9, 4, 622890 }, { 9, 1, 812365 }, { 1, 3, 399349 }, { 10, 2, 407736 },
-                        { 6, 7, 880509 }, { 1, 4, 289656 }, { 8, 0, 802664 }, { 6, 4, 826732 }, { 10, 3, 567982 },
-                        { 5, 6, 434340 }, { 4, 7, 833968 }, { 12, 1, 578047 }, { 8, 5, 739814 }, { 10, 9, 648073 },
-                        { 1, 6, 679167 }, { 3, 6, 933017 }, { 0, 10, 399226 }, { 1, 11, 915959 }, { 0, 12, 393037 },
-                        { 11, 5, 811057 }, { 6, 2, 100832 }, { 5, 1, 731872 }, { 3, 8, 741455 }, { 2, 9, 835397 },
-                        { 7, 0, 516610 }, { 11, 8, 680504 }, { 3, 11, 455056 }, { 1, 0, 252721 } });
-        int len = graph.shortestPath(9, 3);
-        graph.addEdge(new int[] { 11, 1, 873094 });
-        len = graph.shortestPath(3, 10); // outputs 1393401, but should output 1943345
-        // Everything that is wrong with the second LeetCode test case is wrong because it is too small - likely meaning that I forgot to add something when calculating the cost in the algorithm.
-        System.out.println(len);
     }
 
     // If we are needing to use Djikstra's Algorithm for the shortest path between
     // two nodes, then a convenient way to store this graph is a connectivity list.
     List<Integer>[] connectivityList;
     Set<Integer>[] canReachMe;
+    HashMap<Integer, NodeWithCost> nodes;
 
     // However, to store edge weights, let us also use an adjacency matrix
     int[][] adjacencyMatrix;
@@ -72,6 +62,12 @@ public class Graph {
         adjacencyMatrix = new int[n][n];
         costsToConnect = new int[n][n];
         calculatePath = new boolean[n];
+
+        nodes = new HashMap<>();
+        for (int i=0; i<n; i++) {
+            nodes.put(i, new NodeWithCost(i, 0));
+        }
+
         for (int i = 0; i < calculatePath.length; i++)
             calculatePath[i] = true;
         for (int i = 0; i < n; i++) {
@@ -129,68 +125,37 @@ public class Graph {
      * @return int
      */
     public int shortestPath(int node1, int node2) {
-        // TODO - correct this algorithm
 
         if (calculatePath[node1]) { // We have some work to do.
             // Find the shortest path from every node to every other node that it can reach
-            TreeSet<NodeWithCost> addNext = new TreeSet<>(NodeWithCost.NODE_COMPARATOR);
+            PriorityQueue<NodeWithCost> addNext = new PriorityQueue<>(NodeWithCost.NODE_COMPARATOR);
             addNext.add(new NodeWithCost(node1, 0));
-            HashMap<Integer, NodeWithCost> nodeMap = new HashMap<>(); // To avoid recreating NodeWithCost objects
             HashSet<Integer> confirmed = new HashSet<>();
-
-            int next;
+            HashSet<Integer> added = new HashSet<>();
+            
             while (!addNext.isEmpty()) {
-                next = addNext.pollFirst().nodeID;
-                confirmed.add(next);
-                for (int connected : connectivityList[next]) {
-                    if (confirmed.contains(connected)) // not interested
-                        continue;
+                NodeWithCost next = addNext.poll();
+                confirmed.add(next.nodeID);
+                costsToConnect[node1][next.nodeID] = next.cost;
 
-                    // First, find a new node this node can directly reach - and update everyone's
-                    // shortest paths to that node accordingly
-                    int weightOfConnection = adjacencyMatrix[next][connected];
-                    if (costsToConnect[next][connected] == -1) {
-                        costsToConnect[next][connected] = weightOfConnection;
-                    } else { // One may think that since it's only one edge, it's the best connection, but
-                             // not if it's a super weighty edge.
-                        costsToConnect[next][connected] = Math.min(weightOfConnection,
-                                costsToConnect[next][connected]);
-                    }
-                    canReachMe[connected].add(next); // No duplicates will occur - it is a set
-                    // Everything that can reach 'next' can now reach 'connected'. If they already
-                    // could, see if this new way through 'next' is shorter.
-                    for (int canReachNext : canReachMe[next]) {
-                        canReachMe[connected].add(canReachNext);
-                        int newPathWeight = costsToConnect[canReachNext][next] + costsToConnect[next][connected];
-                        if (costsToConnect[canReachNext][connected] == -1) {
-                            costsToConnect[canReachNext][connected] = newPathWeight;
+                // Now add all of the non-confirmed neighbors
+                for (int neighbor : connectivityList[next.nodeID]) {
+                    if (!confirmed.contains(neighbor)) {
+                        NodeWithCost node = nodes.get(neighbor);
+                        if (added.contains(neighbor)) {
+                            // then we need to remove it from the heap, modify it's cost, and re-enter it into the heap
+                            addNext.remove(node);
+                            node.cost = Math.min(node.cost, next.cost + adjacencyMatrix[next.nodeID][neighbor]);
                         } else {
-                            costsToConnect[canReachNext][connected] = Math
-                                    .min(costsToConnect[canReachNext][connected], newPathWeight);
+                            node.cost = next.cost + adjacencyMatrix[next.nodeID][neighbor];
                         }
+                        addNext.add(node);
+                        added.add(neighbor);
                     }
-
-                    // Now we need to add this connection to our heap
-                    if (nodeMap.containsKey(connected)) {
-                        NodeWithCost connection = nodeMap.get(connected);
-                        if (connection.cost > costsToConnect[node1][connected]) {
-                            addNext.remove(connection);
-                            connection.setCost(costsToConnect[node1][connected]);
-                            addNext.add(connection);
-                        }
-                    } else {
-                        NodeWithCost connection = new NodeWithCost(connected, costsToConnect[node1][connected]);
-                        nodeMap.put(connected, connection);
-                        addNext.add(connection);
-                    }
-
                 }
             }
-
-            for (int exploredNode : nodeMap.keySet()) {
-                calculatePath[exploredNode] = false;
-            }
         }
+        calculatePath[node1] = false;
         return costsToConnect[node1][node2];
     }
 
