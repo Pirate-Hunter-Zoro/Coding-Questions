8c7a59099323da4324eb53a08523c81b74ccd7f2
Mikey Ferguson <mikeyferg49@gmail.com>
2024-01-30 12:59:11 -0600

    Need to start over on BranchManager

--- BranchManager/src/Main.java
+++ BranchManager/src/Main.java
@@ -7,7 +7,7 @@ import java.util.*;
  */
 public class Main {
 
-    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedReader reader;
 
     static int[] nextIntArray() throws IOException {
         return Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
@@ -18,89 +18,16 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
+        if (args.length > 0) {
+            String fileName = args[0];
+            File toReadFrom = new File("resources/data/secret/" + fileName);
+            reader = new BufferedReader(new FileReader(toReadFrom));
+        } else {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+        }
+
         int[] nm = nextIntArray();
         int cities = nm[0];
         int people = nm[1];
-
-        Node.refresh();
-
-        for (int i=0; i<cities-1; i++) {
-            int[] fromTo = nextIntArray();
-            Node.add(fromTo[0]);
-            Node.add(fromTo[1]);
-            Node.addRoad(fromTo[0], fromTo[1]);
-        }
-
-        int reached = 0;
-        for (int i=0; i<people; i++) {
-            int destination = nextInt();
-            if (Node.reach(destination))
-                reached++;
-            else
-                break;
-        }
-
-        System.out.println(reached);
     }
-
-    static class Node {
-        private int id;
-        private boolean reachedLastTime;
-        private boolean closed;
-        private Node parent;
-        private List<Node> children;
-
-        public static HashMap<Integer, Node> nodes = new HashMap<>();
-
-        public static void refresh() {
-            nodes = new HashMap<>();
-        }
-
-        public static void add(int id) {
-            if (!nodes.containsKey(id)) {
-                nodes.put(id, new Node(id));
-            }
-        }
-
-        private Node(int id) {
-            this.id = id;
-            closed = false;
-            this.children = new ArrayList<>();
-        }
-
-        public static void addRoad(int from, int to) {
-            nodes.get(from).children.add(nodes.get(to));
-            nodes.get(to).parent = nodes.get(from);
-        }
-
-        private void close() {
-            if (!closed) {
-                closed = true;
-                for (Node child : children)
-                    child.close();
-            }
-        }
-
-        public static boolean reach(int id) {
-            Node destination = nodes.get(id);
-            if (destination.closed)
-                return false;
-            else if (destination.reachedLastTime)
-                return true;
-
-            destination.reachedLastTime = true;
-            if (destination.parent != null) {
-                for (Node sibling : destination.parent.children) {
-                    if (sibling != destination && sibling.id < destination.id) {
-                        sibling.close();
-                    } else {
-
-                    }
-                }
-                reach(destination.parent.id);
-            } 
-            return true;
-        }
-    }
-
 }
