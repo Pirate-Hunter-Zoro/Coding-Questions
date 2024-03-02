a06b7ab51c75796dc624773d0037d6b8ac0eca01
Mikey Ferguson <mikeyferg49@gmail.com>
2024-03-01 19:52:15 -0600

    Need to fix CBTInsert

---
+++ LeetCode/src/CBTInsert.py
@@ -0,0 +1,82 @@
+from TreeNode import TreeNode
+
+class CBTInserter(object):
+    """_summary_
+
+    Args:
+        object (_type_): _description_
+    """
+
+    """
+    A complete binary tree is a binary tree in which every level, except possibly the last, is completely filled, and all nodes are as far left as possible.
+
+    Design an algorithm to insert a new node to a complete binary tree keeping it complete after the insertion.
+
+    Implement the CBTInserter class:
+
+    - CBTInserter(TreeNode root) Initializes the data structure with the root of the complete binary tree.
+    - int insert(int v) Inserts a TreeNode into the tree with value Node.val == val so that the tree remains complete, and returns the value of the parent of the inserted TreeNode.
+    - TreeNode get_root() Returns the root node of the tree.
+    
+    Link:
+    https://leetcode.com/problems/complete-binary-tree-inserter/description/
+    """
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.root = root
+        self.current_level = [root]
+        layer = [root]
+        while (len(layer)>0):
+            next = []
+            for n in layer:
+                if n.left != None:
+                    next.append(n.left)
+                else:
+                    break
+
+                if n.right != None:
+                    next.append(n.right)
+                else:
+                    break
+            if layer[len(layer)-1].right != None:
+                self.current_level = layer
+                layer = next
+            else:
+                break
+        idx = 0
+        while self.current_level[idx].right != None:
+            idx += 1
+        self.add_children_index = idx
+
+    def insert(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+        addChild = self.current_level[self.add_children_index]
+        if addChild.left == None:
+            addChild.left = TreeNode(val)
+        else:
+            addChild.right = TreeNode(val)
+            self.add_children_index += 1
+            if self.add_children_index >= len(self.current_level):
+                next_level = []
+                for node in self.current_level:
+                    next_level.append(node.left)
+                    next_level.append(node.right)
+                self.current_level = next_level
+                self.add_children_index = 0
+        return addChild.val
+
+    def get_root(self):
+        """
+        :rtype: TreeNode
+        """
+        return self.root
+
+root = TreeNode.make_node([1, 2, 3, 4, 5, 6])
+inserter = CBTInserter(root)
+inserter.insert(7)
+inserter.insert(8)
---
+++ LeetCode/src/TreeNode.py
@@ -0,0 +1,50 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    @staticmethod
+    def make_node(val_list: list[int]):
+        root = TreeNode(val_list[0])
+        idx = 1
+        node_map = {}
+        node_map[0] = root
+        for i in range(1, len(val_list)):
+            v = val_list[i]
+            if v is not None:
+                node_map[i] = TreeNode(v)
+
+        parent_index = 0
+        while idx < len(val_list) and parent_index < len(val_list):
+            parent = node_map[parent_index]
+            v = val_list[idx]
+            if v is not None:
+                parent.left = node_map[idx]
+            idx += 1
+            if idx < len(val_list):
+                v = val_list[idx]
+                if v is not None:
+                    parent.right = node_map[idx]
+            idx += 1
+            parent_index += 1
+            while parent_index < len(val_list) and not parent_index in node_map.keys():
+                parent_index += 1
+
+        return root
+
+    def __eq__(self, __value: object) -> bool:
+        if not isinstance(__value, self.__class__):
+            return False
+
+        if self.val == __value.val:
+            if self.left == None and self.right == None:
+                return __value.left == None and __value.right == None
+            elif self.left == None:
+                return __value.left == None and self.right == __value.right
+            elif self.right == None:
+                return __value.right == None and self.left == __value.left
+            else:
+                return self.left == __value.left and self.right == __value.right
+        else:
+            return False
